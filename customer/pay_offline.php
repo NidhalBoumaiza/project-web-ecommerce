99a2db11f25e4f4571ef7348868d45e043764b14
Nidhal Boumaiza <91366911+NidhalBoumaiza@users.noreply.github.com>
2024-05-17 22:00:07 +0100

    first commit

---
+++ customer/pay_offline.php
@@ -0,0 +1,49 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> Cash Details: </th>
+
+<th> Western Union Details: </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name:STB No:123456789 ZIP Code:4000 	 </td>
+
+<td> NIC#123456789 Mobile No:789456123, Name:Nidhalboumaiza </td>
+
+<td> Full Name:Nidhal boumaiza, Mobile No:+21612345678, Name:Nidhal, Country:TN
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
